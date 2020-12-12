aaa165f10c026d9fead45f9a58e4ff052ee52087
Esten Rye <esten.rye@ryezone.com>
2020-12-12 16:02:28 -0600

    change extract location of packer.

--- .cicd/install-packer.sh
+++ .cicd/install-packer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
 set -e
-mkdir -p .local .local/bin
 curl https://releases.hashicorp.com/packer/1.6.5/packer_1.6.5_linux_amd64.zip -o packer.zip
-unzip packer.zip -d ./.local/bin
+sudo unzip packer.zip -d /usr/bin
--- .cicd/run-makefile.sh
+++ .cicd/run-makefile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 set -e
-PATH=${PATH}:${PWD}/.local/bin sudo make --directory=./${CIRCLE_PROJECT_REPONAME} build-virtualbox
+sudo make --directory=${BUILD_SOURCESDIRECTORY} build-virtualbox
